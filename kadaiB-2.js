43894c0e555df5c6cfb69f7a0816bb1104486961
g19922ma <52483478+g19922ma@users.noreply.github.com>
2021-01-15 23:54:56 +0900

    Update kadaiB-2.js

--- kadaiB-2.js
+++ kadaiB-2.js
@@ -1,7 +1,7 @@
      let myButton = document.querySelector('button');
      let myHeading = document.querySelector('h1');
      function setUserName() {
-     let myName = prompt('あなたの名前を入力してください。');
+     let myName = prompt('占いたい人の名前を入力してください。');
      localStorage.setItem('name', myName);
      myHeading.textContent = myName+'さんの運勢の運勢は・・・';
      }
