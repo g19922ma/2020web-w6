cba0030e8d540aacfcffe2a970163a5c53990d32
g19922ma <52483478+g19922ma@users.noreply.github.com>
2021-01-16 22:16:13 +0900

    Update kadaiB-2.js

--- kadaiB-2.js
+++ kadaiB-2.js
@@ -5,7 +5,7 @@
      localStorage.setItem('name', myName);
      myHeading.textContent = myName+'さんの今年の運勢は・・・';
      }
-     myButton.style.border = "dashed 3px rgb(232,145,163)";
+     myButton.style.border = "dashed 3px white";
      myButton.style.backgroundColor = "rgb(240,144,141)";
      myButton.onclick = function() {
      setUserName();
