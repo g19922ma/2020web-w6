fcc050232f95d9dc85b910d7898e9b883311ff6f
g19922ma <52483478+g19922ma@users.noreply.github.com>
2021-01-16 22:04:07 +0900

    Update kadaiB-2.js

--- kadaiB-2.js
+++ kadaiB-2.js
@@ -4,7 +4,6 @@
      let myName = prompt('占いたい人の名前を入力してください。');
      localStorage.setItem('name', myName);
      myHeading.textContent = myName+'さんの今年の運勢は・・・';
-     <script src="kadaiB-3.js"></script>
      }
      myButton.style.border = "dashed 3px rgb(232,145,163)";
      myButton.style.backgroundColor = "rgb(240,144,141)";
