30bf0832e0371cf1b2bc4f4eb796bf28759b1bba
g19922ma <52483478+g19922ma@users.noreply.github.com>
2021-01-16 21:33:39 +0900

    Update kadaiB-2.js

--- kadaiB-2.js
+++ kadaiB-2.js
@@ -7,6 +7,7 @@
      }
      myButton.style.border = "dashed 3px rgb(240,144,141)";
      myButton.style.backgroundColor = "rgb(232,145,163)";
+     myButton.style="padding: 5px;"
      myButton.onclick = function() {
      setUserName();
      }
